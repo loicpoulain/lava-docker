5b0efecddea4a8945926893f3f0a5d6facb42ca6
Loic Poulain <loic.poulain@linaro.org>
2017-09-15 12:38:45 +0200

    expose rpc.mount port
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

--- run-docker.sh
+++ run-docker.sh
@@ -8,6 +8,7 @@ sudo docker run -it \
     -v /dev:/dev -v/boot:/boot -v /lib/modules:/lib/modules \
     -p 69:69/udp -p 80:80 -p 3079:3079 -p 5555:5555 -p 5556:5556 \
     -p 2049:2049 -p 111:111 -p 2049:2049/udp -p 111:111/udp \
+    -p 4242:4242 -p 4242:4242/udp \
     -e "HOST_IP=${HOST_IP}" -e "HOST=${HOSTNAME}" \
     -h ${HOSTNAME} --privileged lava-docker
 
