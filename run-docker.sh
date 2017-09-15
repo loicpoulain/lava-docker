d72e4dcb497f52a11ca7b17ad96984a4be4607cf
Loic Poulain <loic.poulain@linaro.org>
2017-09-15 09:28:25 +0200

    expose rpc and nfs port
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

--- run-docker.sh
+++ run-docker.sh
@@ -7,6 +7,7 @@ HOST_IP=192.168.1.23
 sudo docker run -it \
     -v /dev:/dev -v/boot:/boot -v /lib/modules:/lib/modules \
     -p 69:69/udp -p 80:80 -p 3079:3079 -p 5555:5555 -p 5556:5556 \
+    -p 2049:2049 -p 111:111 -p 2049:2049/udp -p 111:111/udp \
     -e "HOST_IP=${HOST_IP}" -e "HOST=${HOSTNAME}" \
     -h ${HOSTNAME} --privileged lava-docker
 
