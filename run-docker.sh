c847f4576f4f6ecb12c2f8ace44b9463fa9d5519
Loic Poulain <loic.poulain@linaro.org>
2018-04-11 08:48:56 +0200

    automatically clean up the container on exit
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

--- run-docker.sh
+++ run-docker.sh
@@ -4,7 +4,7 @@
 HOSTNAME=worker0
 HOST_IP=192.168.1.23
 
-sudo docker run -it \
+sudo docker run -it --rm \
     -v /dev:/dev -v/boot:/boot -v /lib/modules:/lib/modules \
     -p 69:69/udp -p 80:80 -p 3079:3079 -p 5555:5555 -p 5556:5556 \
     -p 2049:2049 -p 111:111 -p 2049:2049/udp -p 111:111/udp \
